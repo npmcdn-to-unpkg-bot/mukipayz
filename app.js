e77dd400082d767dd0c86e5fa915319d36032b31
Michael Hottman <hottmanmichael@gmail.com>
2016-07-01 15:17:58 -0600

    I should not have done this in the master branch, sorry roger, but added app.js file

---
+++ app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+//env
+require('dotenv').config();
+
+//main app
+var express = require('express'),
+    app = express(),
+    logger = require('morgan');
+
+//required routes
+var routes = {
+    landing: require('./routes/landing')
+};
+
+//app middleware
+app.use(logger('dev'));
+app.use(express.static(__dirname+'/public'));
+
+
+//routes middleware
+app.use('/', routes.landing);
+
+const port = process.env.PORT || 3000;
+app.listen(port, function() {
+    console.log("listening on", port);
+    console.log("All other groups in the q2 project suck.");
+});
