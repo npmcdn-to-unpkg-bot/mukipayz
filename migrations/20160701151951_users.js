641d1c3bf4006971812d3c4a0b90bd9f0bbbcaec
Jamie <lanae_j@operamail.com>
2016-07-01 15:28:54 -0600

    migration

---
+++ migrations/20160701151951_users.js
@@ -0,0 +1,14 @@
+'use strict'
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', function(table){
+    table.increments();
+    table.string('first_name');
+    table.string('last_name');
+    table.string('e-mail');
+    table.string('password');
+  });
+};
+
+exports.down = function(knex, Promise) {
+return knex.schema.dropTable('users');
+};
