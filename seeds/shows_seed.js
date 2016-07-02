4eeca76399273f8b9e11142b079827fa235a7810
Jamie <lanae_j@operamail.com>
2016-07-02 12:36:00 -0600

    added seed file and fixed email

--- seeds/shows_seed.js
+++ seeds/shows_seed.js
@@ -1,11 +1,12 @@
 'use strict';
+
 exports.seed = function(knex, Promise) {
   return knex('users').del()
   .then(function(){  //insert seed entries one by one
     return knex('users').insert({
       first_name: "tiffany",
       last_name: "coils",
-      e-mail: "tiffcoils@garage.com",
+      email: "tiffcoils@garage.com",
       password: "43sdf45rh??sdfgdfag"
     });
 
