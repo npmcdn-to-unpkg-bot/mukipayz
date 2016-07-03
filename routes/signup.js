d43f3f4f8997d5cc9d1f514cb2e9768714b4f994
Jamie <lanae_j@operamail.com>
2016-07-03 11:06:45 -0600

    fixed merge error

--- routes/signup.js
+++ routes/signup.js
@@ -8,45 +8,19 @@ var promise_result = require('../promise');
 // get login page
 
 router.get('/', function(req, res, next) {
-<<<<<<< HEAD
+
     knex('users').then(function(data) {
         res.render('pages/signup', {
             data: data
         });
     }).catch(next);
-=======
 
-  knex('users').then(function(data){
-    res.render('pages/signup', {
-      data: data
-    });
-  }). catch(next);
->>>>>>> login_hashing
-
-});
 
 
 // allow users to log in with a hashed password.
 //by using a required promise.
 router.post('/', function(req, res, next) {
-<<<<<<< HEAD
-    // req.body is undefined for some reason (?)
-    console.log(req.body);
-    promise_result.hash(req.body.password).then(function(result) {
 
-        knex('users').insert({
-            first_name: req.body.first_name,
-            last_name: req.body.last_name,
-            password: result,
-            email: req.body.email,
-
-        }).then(function(data) {
-            res.json({
-                success: true,
-                message: 'ok'
-            });
-        }).catch(next);
-=======
   // req.body is undefined for some reason (?)
 
 promise_result(req.body.password).then(function(result){
@@ -61,7 +35,7 @@ promise_result(req.body.password).then(function(result){
                 res.redirect('/login');
             }).catch(next);
         });
->>>>>>> login_hashing
+
     });
 });
 
