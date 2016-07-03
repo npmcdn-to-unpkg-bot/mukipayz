64db5921f8029c47c2a8a9a8fc8e3e9d5e84f169
Jamie <lanae_j@operamail.com>
2016-07-02 21:12:25 -0600

    made some basic things

--- routes/login.js
+++ routes/login.js
@@ -8,7 +8,7 @@ var promise_result= require('./promise');
 
 router.get('/', function(req, res, next) {
   knex('users').then(function(data){
-    res.render('pages/login', { title: 'hmm' });
+    res.render('pages/login', { title: 'login' });
     //res.send(data);
     // res.render('/login', {
     //   data:data
@@ -19,7 +19,8 @@ router.get('/', function(req, res, next) {
 //things to pass along to team
 //in ejs input fields we need name to equal first_name, last_name and
 // email to match this.
-router.post('pages/login', function(req, res, next) {
+router.post('/', function(req, res, next) {
+  // this is empty for some reason (?)
 console.log(req.body);
 promise_result.hash(req.body.password).then(function(result){
 
