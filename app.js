5d3312aaa7cd7c4f2da3a0ff02d9b6c3170eb2db
Michael Hottman <hottmanmichael@gmail.com>
2016-07-05 09:32:46 -0600

    added cookie session functionality

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ require('dotenv').config();
 //main app
 var express = require('express'),
     app = express(),
-
     logger = require('morgan'),
     path = require('path'),
     methodOverride = require('method-override'),
     bodyParser = require('body-parser'),
-    cookieSession = require("cookie-session");
+    cookieSession = require("cookie-session"),
+    passport = require('passport');
 //setup views
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -30,6 +30,14 @@ app.use(logger('dev'));
 app.use(express.static(__dirname+'/public'));
 app.use(methodOverride('_method'));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(cookieSession({
+    name: 'mukipayz',
+    keys: [process.env.SECRET_ONE, process.env.SECRET_TWO]
+}));
+
+
 //routes middleware
 app.use('/signup', routes.signup);
 app.use('/login', routes.login);
