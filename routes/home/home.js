f45cd1a82e427545ce3949e5a5e3ac0a0e1c278e
Jamie <lanae_j@operamail.com>
2016-07-05 23:13:29 -0600

    so many more pages working

--- routes/home/home.js
+++ routes/home/home.js
@@ -71,28 +71,31 @@ router.get('/group/:id', function(req, res, next) {
         knex('bills').where({group_id:Number(req.params.id)}),
         knex('messages_in_group').where({group_id:Number(req.params.id)})
     ).then(function(data) {
+      res.render('pages/group', {
+          data: data
+      });
 
         //Promise.join will join the data of multiple promises
             //So data[0] == bills array, data[1] == messages in that group
         //data = [all-bills, all-messages] for that id
         //try res.json(data); to see all data returned
-        var joined = {
-            bills: data[0],
-            messages: data[1]
-        };
-
-        if (joined.bills.length === 0) {
-            joined.bills = null;
-        }
-        if (joined.messages.length === 0) {
-            joined.messages = null;
-        }
+        // var joined = {
+        //     bills: data[0],
+        //     messages: data[1]
+        // };
+        //
+        // if (joined.bills.length === 0) {
+        //     joined.bills = null;
+        // }
+        // if (joined.messages.length === 0) {
+        //     joined.messages = null;
+        // }
         //res.send(joined.bills);
     //     // res.json(joined.bills);
     //     // to use in view, data.bills or data.messages
-        res.render('pages/group', {
-            data: joined
-        });
+        // res.render('pages/group', {
+        //     data: joined
+        // });
         //console.log(data);
     });
 
